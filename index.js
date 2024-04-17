b371d736b57cd4a8d5012dd9fb5eaca58f1527ed
SidneyStablein <156612706+SidneyStablein@users.noreply.github.com>
2024-04-17 01:10:31 +0000

    updated css index.html and index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add JavaScript below
+function clickOmatic() {
+    alert('Hello!')
+}
+
+const click = document.getElementById('click');
+
+click.addEventListener("click", (event) => {
+    clickOmatic();
+})
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
